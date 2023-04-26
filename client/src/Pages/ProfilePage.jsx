1482167f08ce3891bd23b3570d308b0cb45963b2
Saif Ali Khan <pythonbytes75@gmail.com>
2023-04-26 16:03:12 +0500

    User Profile Page Module version 1

--- client/src/Pages/ProfilePage.jsx
+++ client/src/Pages/ProfilePage.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import "../styles/Pages/ProfilePage.css"
+import BannerImg from "../Data/Images/banner.png"
+import Avatar from "../Data/Images/avatarProfile.png"
+import {User} from "../Test Data/CurrentUser"
+
 function ProfilePage() {
   return (
     <div className='profile-page'>
-      <h1>Profile Page</h1>
+        {/* userInfo */}
+        <div className="top">
+          <div 
+          id="banner"
+          style={{backgroundImage: `url(${User.banner})`}}
+          ></div>
+
+          <div className="user-wrapper">
+            <div id="avatar" 
+              style={{backgroundImage: `url(${User.avatar})`}}
+            ></div>
+            <h1>{User.name}</h1>
+          </div>
+
+          <div className="info">
+            <h6>Im a {User.occupation}</h6>
+            <h6>{User.age}yrs, {User.gender}, {User.status}</h6>
+            <h6>Live in {User.city}, {User.country}</h6>
+            <span id='join'>Decentrian since {User.date_joined}</span>
+          </div>
+
+          <div className="following">
+            <div id="left">Followers: {User.followers}</div>
+            <div id="right">Following: {User.following}</div>
+          </div>
+        </div>
+      {/* User Feed */}
+      <div className="bottom"></div>
     </div>
   )
 }
---
+++ client/src/Test Data/CurrentUser.js
@@ -0,0 +1,17 @@
+import img1 from "../Data/Test Data/CurrentUser/Ted.png"
+import img2 from "../Data/Test Data/CurrentUser/banner.jpg"
+
+export const User = {
+    name: "Hamza Azhar",
+    avatar: img1,
+    banner: img2,
+    age: 23,
+    gender: "Male",
+    status: "Single",
+    country: "Pakistan",
+    city: "Islamabad",
+    occupation: "Software engineer",
+    date_joined: "April 26th",
+    followers: 102,
+    following: 69
+}
