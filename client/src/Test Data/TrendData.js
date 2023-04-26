fa92e7d031e79556d428c6558629156f684eaea0
hamzaazhar1 <hamzaazharx700i@gmail.com>
2023-04-27 02:20:10 +0500

    Trends appear using test data

---
+++ client/src/Test Data/TrendData.js
@@ -0,0 +1,38 @@
+export const trends=[
+    {
+        id:1,
+        name:"JusticeForSaif",
+        location:"Pakistan",
+        catagoryName:null,
+        type:1,
+        retweets:"25k"
+
+    },
+    {
+        id:2,
+        name:"WhyDidThisHappen",
+        location:"Pakistan",
+        catagoryName:null,
+        type:1,
+        retweets:"15k"
+
+    },
+    {
+        id:3,
+        name:"Assault",
+        location:"Pakistan",
+        catagoryName:"Social Issues",
+        type:0,
+        retweets:"10k"
+
+    },
+    {
+        id:4,
+        name:"IIUI",
+        location:"Pakistan",
+        catagoryName:null,
+        type:1,
+        retweets:"5k"
+
+    }
+];  
--- client/src/components/Trend.jsx
+++ client/src/components/Trend.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import "../styles/Trend.css";
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { Button } from '@mui/material';
-const Trend = ({name, location, type,retweets,catagoryName}) => {
+const Trend = ({id,name, location, type,retweets,catagoryName}) => {
     return ( 
-        <div className="trend">
+        <div className="trend" id={id}>
             <div className='trend-top'>
             {type===0?<span>Trending in {catagoryName}</span>:<span>Trending in {location}</span>}
-            <Button className='button'>
-                <MoreHorizIcon/>
-            </Button>
+            
+            <div>
+                <Button className='button'>
+                    <MoreHorizIcon/>
+                </Button>
+            </div>
             
             </div>
             <span style={
