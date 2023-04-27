10cd4ec9bb2c3b17266c1847ab226ab34d662180
hamzaazhar1 <hamzaazharx700i@gmail.com>
2023-04-27 05:56:24 +0500

    tweet box with limited functionality

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "../styles/Pages/HomePage.css"
+import TweetBox from '../components/TweetBox'
 function HomePage() {
   return (
     <div className='home-page'>
       <h1>Home Page</h1>
+      <TweetBox/>
     </div>
   )
 }
---
+++ client/src/components/TweetBox.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import '../styles/TweetBox.css';
+import { useState, useRef } from 'react';
+import {Avatar,Button} from '@mui/material'
+import CropOriginalIcon from '@mui/icons-material/CropOriginal';
+import GifIcon from '@mui/icons-material/Gif';
+import PollIcon from '@mui/icons-material/Poll';
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
+import CloseIcon from '@mui/icons-material/Close';
+const TweetBox = (profile) => {
+
+    const [image, setImage]=useState(null);
+    const imageRef =useRef();
+
+    const onImageChange = (event)=>{
+        if(event.target.files && event.target.files[0]){
+            let img = event.target.files[0];
+            setImage({
+                image:URL.createObjectURL(img)
+            });
+        }
+    }
+
+    const resize=(e)=>{
+        let textarea=document.getElementById("tweet_text");
+        textarea.style.height="auto";
+        textarea.style.height=`${e.target.scrollHeight}px`;
+    }
+    
+    return ( 
+        <div className="tweetbox">
+            <div className="avatar">
+                <Avatar/>
+            </div>
+            <div className="datafield">
+                <div>
+                    <div className="textfield">
+                        <textarea name="tweet" id="tweet_text" col="" rows="" placeholder="What's Happening?" onKeyUp={resize}/>
+                        {image && (
+                            <div className="previewImage">
+                                <CloseIcon onClick={()=>{setImage(null)}}/>
+                                <img src={image.image} alt="" />
+                            </div>
+                        )}
+                    </div>
+                    <div className="bottomfield">
+                        <div className="media_icons">
+                            <CropOriginalIcon className='_options' onClick={()=>{imageRef.current.click()}}/>
+                            <GifIcon className='_options'/>
+                            <PollIcon className='_options'/>
+                            <SentimentSatisfiedIcon className='_options'/>
+                        </div>
+                        <div className="tweet_btn">
+                            <Button className='tweet-button' style={{
+                                backgroundColor: "var(--Brand-color)",
+                                color: "var(--D-font-color)",
+                                borderRadius:"25px",
+                                padding:"5px 10px",
+                                fontWeight:"bold"
+                            }}>Tweet</Button>
+                            <div style={{
+                                display:"none"
+                            }}>
+                                <input type="file" name="myImage" ref={imageRef} onChange={onImageChange}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+     );
+}
+ 
+export default TweetBox;
