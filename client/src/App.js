9f6d894cfabb8e8e4c90993f720ff8ea19f30f1d
Saif Ali Khan <pythonbytes75@gmail.com>
2023-05-04 16:18:53 +0500

    Dark mode feature final version!

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,42 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 import Widgets from './components/Widgets';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [darkModeEnabled, setDarkModeEnabled] = useState(true);
+
+  useEffect(() => {
+    const darkModeEnabledFromStorage = localStorage.getItem('darkModeEnabled');
+    if (darkModeEnabledFromStorage !== null) {
+      setDarkModeEnabled(JSON.parse(darkModeEnabledFromStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!darkModeEnabled) {
+      document.documentElement.style.setProperty('--background-color', 'var(--LightMode)');
+      document.documentElement.style.setProperty('--font-color', 'var(--L-font-color)');
+      document.documentElement.style.setProperty('--card-color', 'var(--H-Light-background-color)');
+      document.documentElement.style.setProperty('--card-child-color', 'var(--H-Light-L-background-color)');
+      document.documentElement.style.setProperty('--card-hover-color', 'var(--H-Light-hover-background-color)');
+      document.documentElement.style.setProperty('--border-color', 'var(--L-border-color)');
+      document.documentElement.style.setProperty('--active', 'var(--L-active)');
+      document.documentElement.style.setProperty('--glassEffect', 'var(--L-ge)');
+      document.documentElement.style.setProperty('--Dark-color', 'var(--L-color)');
+    } else {
+      document.documentElement.style.setProperty('--background-color', 'var(--DarkMode)');
+      document.documentElement.style.setProperty('--font-color', 'var(--D-font-color)');
+      document.documentElement.style.setProperty('--card-color', 'var(--H-background-color)');
+      document.documentElement.style.setProperty('--card-child-color', 'var(--H-L-background-color)');
+      document.documentElement.style.setProperty('--card-hover-color', 'var(--H-hover-background-color)');
+      document.documentElement.style.setProperty('--border-color', 'var(--D-border-color)');
+      document.documentElement.style.setProperty('--active', 'var( --D-active)');
+      document.documentElement.style.setProperty('--glassEffect', 'var(--D-ge)');
+      document.documentElement.style.setProperty('--Dark-color', 'var(--D-color)');
+    }
+  }, [darkModeEnabled]);
+
   return (
     <div className="App">
       {/* Sidebar */}
--- client/src/Pages/SettingsPage.jsx
+++ client/src/Pages/SettingsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "../styles/Pages/SettingsPage.css"
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { useNavigate } from 'react-router-dom';
@@ -11,32 +11,69 @@ function SettingsPage() {
   const handleOptionClick = (url) => {
     navigate(url);
   };
-  const handleDarkModeToggle = () => {
-    const newDarkModeEnabled = darkModeEnabled;
-    setDarkModeEnabled(!darkModeEnabled);
+  // const handleDarkModeToggle = () => {
+  //   const newDarkModeEnabled = darkModeEnabled;
+  //   setDarkModeEnabled(!darkModeEnabled);
 
-  if (newDarkModeEnabled) {
-    document.documentElement.style.setProperty('--background-color', 'var(--LightMode)');
-    document.documentElement.style.setProperty('--font-color', 'var(--L-font-color)');
-    document.documentElement.style.setProperty('--card-color', 'var(--H-Light-background-color)');
-    document.documentElement.style.setProperty('--card-child-color', 'var(--H-Light-L-background-color)');
-    document.documentElement.style.setProperty('--card-hover-color', 'var(--H-Light-hover-background-color)');
-    document.documentElement.style.setProperty('--border-color', 'var(--L-border-color)');
-    document.documentElement.style.setProperty('--active', 'var(--L-active)');
-    document.documentElement.style.setProperty('--glassEffect', 'var(--L-ge)');
-    document.documentElement.style.setProperty('--Dark-color', 'var(--L-color)');
-  } else {
-    document.documentElement.style.setProperty('--background-color', 'var(--DarkMode)');
-    document.documentElement.style.setProperty('--font-color', 'var(--D-font-color)');
-    document.documentElement.style.setProperty('--card-color', 'var(--H-background-color)');
-    document.documentElement.style.setProperty('--card-child-color', 'var(--H-L-background-color)');
-    document.documentElement.style.setProperty('--card-hover-color', 'var(--H-hover-background-color)');
-    document.documentElement.style.setProperty('--border-color', 'var(--D-border-color)');
-    document.documentElement.style.setProperty('--active', 'var( --D-active)');
-    document.documentElement.style.setProperty('--glassEffect', 'var(--D-ge)');
-    document.documentElement.style.setProperty('--Dark-color', 'var(--D-color)');
-  }
+  // if (newDarkModeEnabled) {
+  //   document.documentElement.style.setProperty('--background-color', 'var(--LightMode)');
+  //   document.documentElement.style.setProperty('--font-color', 'var(--L-font-color)');
+  //   document.documentElement.style.setProperty('--card-color', 'var(--H-Light-background-color)');
+  //   document.documentElement.style.setProperty('--card-child-color', 'var(--H-Light-L-background-color)');
+  //   document.documentElement.style.setProperty('--card-hover-color', 'var(--H-Light-hover-background-color)');
+  //   document.documentElement.style.setProperty('--border-color', 'var(--L-border-color)');
+  //   document.documentElement.style.setProperty('--active', 'var(--L-active)');
+  //   document.documentElement.style.setProperty('--glassEffect', 'var(--L-ge)');
+  //   document.documentElement.style.setProperty('--Dark-color', 'var(--L-color)');
+  // } else {
+  //   document.documentElement.style.setProperty('--background-color', 'var(--DarkMode)');
+  //   document.documentElement.style.setProperty('--font-color', 'var(--D-font-color)');
+  //   document.documentElement.style.setProperty('--card-color', 'var(--H-background-color)');
+  //   document.documentElement.style.setProperty('--card-child-color', 'var(--H-L-background-color)');
+  //   document.documentElement.style.setProperty('--card-hover-color', 'var(--H-hover-background-color)');
+  //   document.documentElement.style.setProperty('--border-color', 'var(--D-border-color)');
+  //   document.documentElement.style.setProperty('--active', 'var( --D-active)');
+  //   document.documentElement.style.setProperty('--glassEffect', 'var(--D-ge)');
+  //   document.documentElement.style.setProperty('--Dark-color', 'var(--D-color)');
+  // }
+  // };
+
+  const handleDarkModeToggle = () => {
+    const newDarkModeEnabled = !darkModeEnabled;
+    setDarkModeEnabled(newDarkModeEnabled);
+    localStorage.setItem('darkModeEnabled', newDarkModeEnabled);
   };
+
+  useEffect(() => {
+    const darkModeEnabledFromStorage = localStorage.getItem('darkModeEnabled');
+    if (darkModeEnabledFromStorage !== null) {
+      setDarkModeEnabled(JSON.parse(darkModeEnabledFromStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!darkModeEnabled) {
+      document.documentElement.style.setProperty('--background-color', 'var(--LightMode)');
+      document.documentElement.style.setProperty('--font-color', 'var(--L-font-color)');
+      document.documentElement.style.setProperty('--card-color', 'var(--H-Light-background-color)');
+      document.documentElement.style.setProperty('--card-child-color', 'var(--H-Light-L-background-color)');
+      document.documentElement.style.setProperty('--card-hover-color', 'var(--H-Light-hover-background-color)');
+      document.documentElement.style.setProperty('--border-color', 'var(--L-border-color)');
+      document.documentElement.style.setProperty('--active', 'var(--L-active)');
+      document.documentElement.style.setProperty('--glassEffect', 'var(--L-ge)');
+      document.documentElement.style.setProperty('--Dark-color', 'var(--L-color)');
+    } else {
+      document.documentElement.style.setProperty('--background-color', 'var(--DarkMode)');
+      document.documentElement.style.setProperty('--font-color', 'var(--D-font-color)');
+      document.documentElement.style.setProperty('--card-color', 'var(--H-background-color)');
+      document.documentElement.style.setProperty('--card-child-color', 'var(--H-L-background-color)');
+      document.documentElement.style.setProperty('--card-hover-color', 'var(--H-hover-background-color)');
+      document.documentElement.style.setProperty('--border-color', 'var(--D-border-color)');
+      document.documentElement.style.setProperty('--active', 'var( --D-active)');
+      document.documentElement.style.setProperty('--glassEffect', 'var(--D-ge)');
+      document.documentElement.style.setProperty('--Dark-color', 'var(--D-color)');
+    }
+  }, [darkModeEnabled]);
 
   return (
     <div className='settings'>
