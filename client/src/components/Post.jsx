31eaf22e0f199e1c22f88d378ff73153efb733f9
hamzaazhar1 <hamzaazharx700i@gmail.com>
2023-05-06 15:53:44 +0500

    commentModule input modal finished

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -5,7 +5,19 @@ import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ReplyIcon from '@mui/icons-material/Reply';
+import { useRef } from 'react';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { User } from '../Test Data/CurrentUser';
+import TweetBox from './TweetBox';
 const Post = ({tweet}) => {
+    const cModal=useRef();
+    const postComment=()=>{
+        cModal.current.showModal();
+    }
+
+    const closePostComment=()=>{
+        cModal.current.close();
+    }
     return (  
         <div className="post">
             <div className="post_avatar">
@@ -32,11 +44,51 @@ const Post = ({tweet}) => {
                     )}
                 </div>
                 <div className="post_options">
-                    <ChatBubbleOutlineIcon className='_postoptions'/>
+                    <ChatBubbleOutlineIcon className='_postoptions' onClick={postComment}/>
                     <FavoriteBorderIcon className='_postoptions'/>
                     <ReplyIcon className='_postoptions'/>
                 </div>
             </div>
+            <dialog ref={cModal} className='post_comment_input'>
+                <div className='post_comment_input_display'>
+
+                    <IconButton className="postCommentCloseButton" onClick={closePostComment}>
+                        <CancelIcon className='postCommentClose' />
+                    </IconButton>
+                    <div className="mainPost">
+                        <div className="replyAvatarSide">
+                            <Avatar src={tweet.user.avatar} style={{
+                    width:"2.5em",
+                    height:"2.5em"
+                }}/>
+                            <div className='belowAvatar'>
+                                <div className="straightLine"></div>
+                            </div>
+                            
+                        </div>
+                        <div className="replyBodySide">
+                            <div className='post_top_userinfo'>
+                                <span className='post_username'>{tweet.user.name}</span>
+                                <span className='post_info'>{tweet.user.at}·{tweet.date}</span>
+                            </div>
+                            <div className="replyMaintext">
+                                {tweet.text && (<span className='post_content_text'>
+                                    {tweet.text}
+                                    </span>)}
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div className='replyingTo'>
+                        <div className='belowMainPost'>
+                                    <div className="straightLine"></div>
+                        </div>
+                        <div className="replying">Replying to {tweet.user.at}</div>
+                    </div> */}
+                    <div className="userReply">
+                        <TweetBox profile={User}/>
+                    </div>
+                </div>
+            </dialog>
         </div>
     );
 }
