5bf2a7a98f4dee093be98e4b3eab78302a011434
Saif Ali Khan <pythonbytes75@gmail.com>
2023-05-24 14:29:13 +0500

    Auth module -v2

--- client/src/components/SubSettingsComponents/Account.jsx
+++ client/src/components/SubSettingsComponents/Account.jsx
@@ -4,116 +4,146 @@ import "../../styles/SubSettingsComponents/Account.css"
 import DefaultAvatar from "../../Data/Images/avatar.jpg"
 import EditIcon from '@mui/icons-material/Edit';
 import { _User } from '../../Scripts/UserStorage';
+import config from '../../config';
+import DecentraAbi from '../../abi/Decentra.json';
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal';	
+import { Web3Storage } from 'web3.storage';
 
 function Account() {
-  const loc_user = _User.getUserData();
+	const loc_user = _User.getUserData();
+	const Web3StorageApi = config.REACT_APP_WEB3STORAGE_API_KEY;
 
-  const [name, setName] = useState(loc_user.name);
-  const [age, setAge] = useState(loc_user.age);
-  const [gender, setGender] = useState(loc_user.gender);
-  const [status, setStatus] = useState(loc_user.status);
-  const [country, setCountry] = useState(loc_user.country);
-  const [city, setCity] = useState(loc_user.city);
-  const [occupation, setOccupation] = useState(loc_user.occupation);
-  const [avatar, setAvatar] = useState(loc_user.avatar);
-  const [banner, setBanner] = useState(loc_user.banner);
+	const [name, setName] = useState(loc_user.name);
+	const [age, setAge] = useState(loc_user.age);
+	const [gender, setGender] = useState(loc_user.gender);
+	const [status, setStatus] = useState(loc_user.status);
+	const [country, setCountry] = useState(loc_user.country);
+	const [city, setCity] = useState(loc_user.city);
+	const [occupation, setOccupation] = useState(loc_user.occupation);
+	const [avatar, setAvatar] = useState(loc_user.avatar);
+	const [banner, setBanner] = useState(loc_user.banner);
+	let banner_URL = null;
+	let avatar_URL = null;
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
+	const handleNameChange = (event) => {
+		setName(event.target.value);
+	};
 
-  const handleAgeChange = (event) => {
-    setAge(event.target.value);
-  };
+	const handleAgeChange = (event) => {
+		setAge(event.target.value);
+	};
 
-  const handleGenderChange = (event) => {
-    setGender(event.target.value);
-  };
+	const handleGenderChange = (event) => {
+		setGender(event.target.value);
+	};
 
-  const handleStatusChange = (event) => {
-    setStatus(event.target.value);
-  };
+	const handleStatusChange = (event) => {
+		setStatus(event.target.value);
+	};
 
-  const handleCountryChange = (event) => {
-    setCountry(event.target.value);
-  };
+	const handleCountryChange = (event) => {
+		setCountry(event.target.value);
+	};
 
-  const handleCityChange = (event) => {
-    setCity(event.target.value);
-  };
+	const handleCityChange = (event) => {
+		setCity(event.target.value);
+	};
 
-  const handleOccupationChange = (event) => {
-    setOccupation(event.target.value);
-  };
+	const handleOccupationChange = (event) => {
+		setOccupation(event.target.value);
+	};
 
-  const handleAvatarChange = (event) => {
-    setAvatar(URL.createObjectURL(event.target.files[0]));
-  };
+	const handleAvatarChange = (event) => {
+		// setAvatar(URL.createObjectURL(event.target.files[0]));
+		
+	};
 
-  const handleBannerChange = (event) => {
-    setBanner(URL.createObjectURL(event.target.files[0]));
-  };
+	const handleBannerChange = (event) => {
+		// setBanner(URL.createObjectURL(event.target.files[0]));
+	};
 
-  return (
-    <div className='account'>
-       <div className="account_header">Account</div>
-       <div className="form-images" style={{backgroundImage: `url(${banner})`}}>
-        <div id="form-avatar" style={{backgroundImage: `url(${avatar || DefaultAvatar})`}}></div>
-       </div>
-      <h6><span>AccountID:</span> {loc_user.account_id}</h6>
-      
-       <div className="form">
-        <h6><EditIcon />Edit Profile:</h6>
-        <div className="form-fields">
-          <div className='form-duo'>
-            <label>Name</label>
-            <input type="text" value={name} onChange={handleNameChange} />
-          </div>
-          <div className='form-duo'>
-            <label>Occupation</label>
-            <input type="text" value={occupation} onChange={handleOccupationChange} />
-          </div>
-        </div>
 
-        <div className="form-fields">
-          <div className='form-triplet'>
-          <label>Age</label>
-         <input type="number" value={age} onChange={handleAgeChange} />
-          </div>
-          <div className='form-triplet'>
-          <label>Gender</label>
-         <input type="text" value={gender} onChange={handleGenderChange} />
-          </div>
-          <div className='form-triplet'>
-          <label>Status</label>
-         <input type="text" value={status} onChange={handleStatusChange} />
-          </div>    
-        </div>
+	async function storeFile (selectedFile) {
+		try {
+			const client = new Web3Storage({token: Web3StorageApi});
+			const rootCid = await client.put(selectedFile);
+			let ipfsUploadedUrl = `https://${rootCid}.ipfs.dweb.link/${selectedFile[0].name}`;
+			return ipfsUploadedUrl;
+		} catch(e) {
+			alert("UnExpected Error: " + e);
+		}
+		
+	}
 
-        <div className="form-fields">
-          <div className='form-duo'><label>City</label>
-         <input type="text" value={city} onChange={handleCityChange} /></div>
-          <div className='form-duo'>
-          <label>Country</label>
-         <input type="text" value={country} onChange={handleCountryChange} /> 
-          </div>
-        </div>
+	async function updateUser() {
+		if(avatar !== null) {
 
-        <div className="form-fields">
-          <div className='form-duo'>
-            <label>Avatar</label>
-            <input type="file" accept="image/*" onChange={handleAvatarChange} />
-          </div>
-          <div className='form-duo'>
-          <label>Banner</label>
-        <input type="file" accept="image/*" onChange={handleBannerChange} />
-          </div>
-        </div>
-       </div>
+		}
+		if(banner !== null) {
+			
+		}
+	}
+	return (
+		<div className='account'>
+		<div className="account_header">Account</div>
+		<div className="form-images" style={{backgroundImage: `url(${banner})`}}>
+			<div id="form-avatar" style={{backgroundImage: `url(${avatar || DefaultAvatar})`}}></div>
+		</div>
+		<h6><span>AccountID:</span> {loc_user.account_id}</h6>
+		
+		<div className="form">
+			<h6><EditIcon />Edit Profile:</h6>
+			<div className="form-fields">
+			<div className='form-duo'>
+				<label>Name</label>
+				<input type="text" value={name} onChange={handleNameChange} />
+			</div>
+			<div className='form-duo'>
+				<label>Occupation</label>
+				<input type="text" value={occupation} onChange={handleOccupationChange} />
+			</div>
+			</div>
 
-       <div className="form-save"><button>Save</button></div>
-    </div>
-  )
+			<div className="form-fields">
+			<div className='form-triplet'>
+			<label>Age</label>
+			<input type="number" value={age} onChange={handleAgeChange} />
+			</div>
+			<div className='form-triplet'>
+			<label>Gender</label>
+			<input type="text" value={gender} onChange={handleGenderChange} />
+			</div>
+			<div className='form-triplet'>
+			<label>Status</label>
+			<input type="text" value={status} onChange={handleStatusChange} />
+			</div>    
+			</div>
+
+			<div className="form-fields">
+			<div className='form-duo'><label>City</label>
+			<input type="text" value={city} onChange={handleCityChange} /></div>
+			<div className='form-duo'>
+			<label>Country</label>
+			<input type="text" value={country} onChange={handleCountryChange} /> 
+			</div>
+			</div>
+
+			<div className="form-fields">
+			<div className='form-duo'>
+				<label>Avatar</label>
+				<input type="file" accept="image/*" onChange={handleAvatarChange} />
+			</div>
+			<div className='form-duo'>
+			<label>Banner</label>
+			<input type="file" accept="image/*" onChange={handleBannerChange} />
+			</div>
+			</div>
+		</div>
+
+		<div className="form-save"><button>Save</button></div>
+		</div>
+	)
 }
 
 export default Account
