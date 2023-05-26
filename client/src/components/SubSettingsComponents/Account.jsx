cfea601014ea0fcfbd07750dd2e3c8dacbc67f49
Saif Ali Khan <pythonbytes75@gmail.com>
2023-05-26 11:52:15 +0500

    User Profile is now modifiable -v3 next -v4 will have ui changed

--- client/src/components/SubSettingsComponents/Account.jsx
+++ client/src/components/SubSettingsComponents/Account.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import "../../styles/SubSettingsComponents/Account.css"
-// import {User} from "../../Test Data/CurrentUser"
-import DefaultAvatar from "../../Data/Images/avatar.jpg"
 import EditIcon from '@mui/icons-material/Edit';
 import { _User } from '../../Scripts/UserStorage';
 import config from '../../config';
@@ -9,12 +7,31 @@ import DecentraAbi from '../../abi/Decentra.json';
 import { ethers } from 'ethers';
 import Web3Modal from 'web3modal';	
 import { Web3Storage } from 'web3.storage';
+import { useNotification } from "@web3uikit/core";
 
 function Account() {
-	const loc_user = _User.getUserData();
+	/* --to show notification-- */
+	const notification = useNotification();
+
+	/* --getting local user data-- */
+	const [loc_user, setLocUser] = useState(_User.getUserData());
+  	useEffect(() => {
+		const handleLocalStorageUpdated = () => {
+		setLocUser(_User.getUserData());
+		};
+
+   	 	window.addEventListener('localStorageUpdated', handleLocalStorageUpdated);
+
+    	return () => {
+      		window.removeEventListener('localStorageUpdated', handleLocalStorageUpdated);
+    	};
+  	}, []);
+
+	/* --APIs used store data in blockchain and ipfs-- */
 	const Web3StorageApi = config.REACT_APP_WEB3STORAGE_API_KEY;
 	const DecentraContractAddress = config.REACT_APP_DECENTRA_CONTRACT_ADDRESS;
 
+	/* --User modifiable fields-- */
 	const [name, setName] = useState(loc_user.name);
 	const [age, setAge] = useState(loc_user.age);
 	const [gender, setGender] = useState(loc_user.gender);
@@ -22,17 +39,20 @@ function Account() {
 	const [country, setCountry] = useState(loc_user.country);
 	const [city, setCity] = useState(loc_user.city);
 	const [occupation, setOccupation] = useState(loc_user.occupation);
-	const [avatar, setAvatar] = useState(loc_user.avatar);
-	const [banner, setBanner] = useState(loc_user.banner);
-	const [prev_avatar, setPrevAvatar] = useState(null);
-	const [prev_banner, setPrevBanner] = useState(null);
-	const [tmp_avatar, setTmpAvatar] = useState();
+	const [avatar, setAvatar] = useState(loc_user.avatar); // initially avatar url & for avatar file array
+	const [banner, setBanner] = useState(loc_user.banner); // initially banner url & for banner file array
+	const [prev_avatar, setPrevAvatar] = useState(null); // preview avatar
+	const [prev_banner, setPrevBanner] = useState(null); // preview banner
+	const [chk_avatar, setChkAvatar] = useState(false); // check if avatar is changed
+	const [chk_banner, setChkBanner] = useState(false); // check if banner is changed
 
+	/* --Ensuring states have the latest value-- */
 	useEffect(() => {
 		setAvatar(loc_user.avatar);
 		setBanner(loc_user.banner);
 	  }, [loc_user.avatar, loc_user.banner]);
-
+	
+	/* --Change Events for all fields-- */
 	const handleNameChange = (event) => {
 		setName(event.target.value);
 	};
@@ -62,23 +82,23 @@ function Account() {
 	};
 
 	const handleAvatarChange = (event) => {
-		// setAvatar(URL.createObjectURL(event.target.files[0]));
 		if(event !=null){
-            // setAvatar(event);
-			setTmpAvatar(event);
+			setAvatar(event.target.files); //file array
 			setPrevAvatar(URL.createObjectURL(event.target.files[0]));
+			setChkAvatar(true);
         }
 		
 	};
 
 	const handleBannerChange = (event) => {
 		if(event != null) {
-		setBanner(event);
+		setBanner(event.target.files); //file array
 		setPrevBanner(URL.createObjectURL(event.target.files[0]));
+		setChkBanner(true);
 		}
 	};
 
-
+	/* --Storing data in ipfs-- */
 	async function storeFile (selectedFile) {
 		try {
 			const client = new Web3Storage({token: Web3StorageApi});
@@ -86,33 +106,28 @@ function Account() {
 			let ipfsUploadedUrl = `https://${rootCid}.ipfs.dweb.link/${selectedFile[0].name}`;
 			return ipfsUploadedUrl;
 		} catch(e) {
-			alert("UnExpected Error: " + e);
+			alert("OOPS :(\n" + e);
 		}
 		
 	}
 
+	/* --Updating user when save btn is clicked-- */
 	async function updateUser() {
-		const loc_user = _User.getUserData(); // get current user data
-		console.log("Avatar: " + avatar);
-		console.log("Banner: " + banner);
-		console.log("LAvatar: " + loc_user.avatar);
-		console.log("LBanner: " + loc_user.banner);
-		// hidding preview
-		// storing banner and avatar in ipfs, setting lattest URL in usestate
-		// if(avatar !== null) {
-		// 	let avatarURL = await storeFile([avatar]);
-		// 	setAvatar(avatarURL);
-		// 	console.log(tmp_avatar);
-		// }
-		// if(banner !== null) {
-		// 	let bannerURL = await storeFile([banner]);
-		// 	setBanner(bannerURL);
-		// }
+		let bannerURL = null;
+		let avatarURL = null;
+
+		if (avatar !== null && avatar !== undefined && chk_avatar) {
+			 avatarURL = await storeFile(avatar);
+		}
+		if (banner !== null && banner !== undefined && chk_banner) {
+			 bannerURL = await storeFile(banner);
+		}
+		
 		// creating new user object
 		const profile = {
 			name:name,
-			avatar:avatar,
-			banner:banner,
+			avatar:avatarURL !== null ? avatarURL : loc_user.avatar,
+			banner:avatarURL !== null ? avatarURL : loc_user.banner,
 			age:age,
 			gender:gender,
 			status:status,
@@ -128,7 +143,7 @@ function Account() {
 			user_following: loc_user.user_following,
 			user_followed: loc_user.user_followed
 		}
-		console.log(userDetails);
+
 		const web3Modal = new Web3Modal();
 		const connection = await web3Modal.connect();
 		let provider = new ethers.BrowserProvider(connection);
@@ -136,18 +151,32 @@ function Account() {
 		const signer = await provider.getSigner();
 		const signerAddress = await signer.getAddress();
 		const contract = new ethers.Contract(DecentraContractAddress,DecentraAbi.abi,signer);
+		
 		const transaction = await contract.updateUser(userDetails);
-		console.log(transaction);
 		_User.setUserLocalStorage(userDetails, loc_user.active_account);
-		alert("successfull");
+
+		// Show sccuess notification
+		notification({
+			type: 'success',
+			message: 'Profile Updated Successfully',
+			title: 'New Notification',
+			position: 'topR',
+			style: {
+				backgroundColor: 'var(--background-color)', // Set the desired background color
+			  },
+		});
+
 		setPrevAvatar(null);
 		setPrevBanner(null);
 	}
+
+
+
 	return (
 		<div className='account'>
 		<div className="account_header">Account</div>
 		<div className="form-images" style={{backgroundImage: `url(${banner})`}}>
-			<div id="form-avatar" style={{backgroundImage: `url(${avatar || DefaultAvatar})`}}></div>
+			<div id="form-avatar" style={{backgroundImage: `url(${avatar})`}}></div>
 		</div>
 		<h6><span>AccountID:</span> {loc_user.active_account}</h6>
 		
--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 const config = {
     // Remove the key before pushing
-    REACT_APP_WEB3STORAGE_API_KEY: undefined,
-    REACT_APP_DECENTRA_CONTRACT_ADDRESS: undefined,
+    REACT_APP_WEB3STORAGE_API_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDUwRDg5MzNmNDFBMEFBYUExNjNkM2RCN0M5Mjc2Mjg2MENkMGI0OTgiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODQyMzEzNTM0NzUsIm5hbWUiOiJkZWNlbnRyYSJ9.3sQwstetMfasLf9nRZCRrfgutOI1j4HInyDuvTVHlXg",
+    REACT_APP_DECENTRA_CONTRACT_ADDRESS: "0xc0054837B08E87C4c5a310c3fe737DE4FA907392",
 
     // Default images
     // DEFAULT_AVATAR: "https://bafybeihmkhn7vo5f4gdj6lvo6ismlvc3bzbalm6rzevime73yrnn2io3zi.ipfs.dweb.link/avatar.jpg",
