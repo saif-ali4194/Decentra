3ad5c9a28ef42551ac81f5ac4171ab9ad2744c11
Saif Ali Khan <pythonbytes75@gmail.com>
2023-08-23 10:39:00 +0500

    creating a demo branch

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,7 @@ const config = {
     // Remove the key before pushing
     REACT_APP_WEB3STORAGE_API_KEY: null,
     REACT_APP_DECENTRA_CONTRACT_ADDRESS: "0x4655ef9D9528142E899c0204De1593A717724157",
-    REACT_APP_AXIOS:"http://192.168.18.16:3001/upload" //http://10.140.51.24:3001/upload
+    REACT_APP_AXIOS:"http://10.140.49.77:3001/upload" //http://10.140.51.24:3001/upload
 };
 
 export default config;
